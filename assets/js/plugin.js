5fe10bb69cd24918f64428e76bdf69d3f4cf58fa
MowafaqHassan <70487681+MowafaqHassan@users.noreply.github.com>
2020-11-01 15:00:02 -0800

    natex project

---
+++ assets/js/plugin.js
@@ -0,0 +1,39 @@
+$(function() {
+    /* navbar scroll  */
+    $(".navbar-nav .nav-item .nav-link").click(function() {
+        $("html , body").animate({
+                scrollTop: $("#" + $(this).data("value")).offset().top,
+            },
+            1000
+        );
+    });
+
+    /* portfolio filter */
+    $(".list").click(function() {
+        const value = $(this).attr("data-filter");
+        console.log(value);
+        if (value == "All") {
+            $(".project-imgs").show("1000");
+        } else {
+            $(".project-imgs")
+                .not("." + value)
+                .hide("1000");
+
+            $(".project-imgs")
+                .filter("." + value)
+                .show("1000");
+        }
+    });
+
+    $(".team a").click(function($e) {
+        $e.preventDefault();
+    });
+
+    /* contact section */
+    $(".Send-button").click(function($e) {
+        $e.preventDefault();
+        if ($(".form-group input").val() == "") {
+            $(".form-control").css("outline", "1px solid red");
+        }
+    });
+});
